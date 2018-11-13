fc2d8cffbd64c20c8731ebd41b92acd463e0af44
huohaitao <huohaitao1@126.com>
2018-11-13 21:05:51 +0800

    1

---
+++ com.yrtech.SurveyWeb/Controllers/BrandContentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    public class BrandContentController : Controller
+    {
+
+        public ActionResult ShopIndex()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectIndex()
+        {
+            return View();
+        }
+
+        public ActionResult SbjectTypeExamIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ com.yrtech.SurveyWeb/Controllers/ProjectContentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    public class ProjectContentController : Controller
+    {
+        // GET: SubjectContent
+        public ActionResult SubjectIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult SubjectInspectionStandardIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult SubjectFileIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult SubjectLossResultIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult SubjectTypeScoreRegionIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult SubjectLinkIndex()
+        {
+            return View();
+        }
+
+        // GET: SubjectContent
+        public ActionResult ShopByProject()
+        {
+            return View();
+        }
+    }
+}
