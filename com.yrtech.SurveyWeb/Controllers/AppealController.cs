56a498774ab7544fc8fcda4304de10f50668cdcb
huohaitao1 <huohaitao1@126.com>
2019-03-05 23:13:45 +0800

    添加申诉

---
+++ com.yrtech.SurveyWeb/Controllers/AppealController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    public class AppealController : Controller
+    {
+
+        // GET: Appeal
+        public ActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
