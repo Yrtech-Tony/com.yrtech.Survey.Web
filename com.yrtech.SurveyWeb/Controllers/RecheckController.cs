a8af77a40f761d3f4e893d25c9f8c720be47e393
huohaitao1 <huohaitao1@126.com>
2019-04-22 20:37:35 +0800

    1

---
+++ com.yrtech.SurveyWeb/Controllers/RecheckController.cs
@@ -0,0 +1,30 @@
+﻿using com.yrtech.SurveyWeb.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    [AuthenAdmin]
+    public class RecheckController : Controller
+    {
+        //
+        // GET: /Recheck/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(string projectId,string shopId,string subjectRecheckTypeId,string statusName)
+        {
+            ViewBag.ProjectId = projectId;
+            ViewBag.ShopId = shopId;
+            ViewBag.SubjectRecheckTypeId = subjectRecheckTypeId;
+            ViewBag.StatusName = statusName;
+            return View();
+        }
+        
+	}
+}
