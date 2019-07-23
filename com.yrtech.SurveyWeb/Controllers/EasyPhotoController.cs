aa8a8f4d02a2058544d20d150f39c20f600ef9da
Tony <moujunsheng15@163.com>
2019-07-23 21:34:48 +0800

    添加了EasyPhoto

---
+++ com.yrtech.SurveyWeb/Controllers/EasyPhotoController.cs
@@ -0,0 +1,25 @@
+﻿using com.yrtech.SurveyWeb.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    [AuthenAdmin]
+    public class EasyPhotoController : Controller
+    {
+
+        
+        public ActionResult EasyPhotoProjectIndex()
+        {
+            return View();
+        }
+        public ActionResult EasyPhotoProjectEdit()
+        {
+            return PartialView("_PartialEasyPhotoProjectEdit");
+        }
+    }
+}
