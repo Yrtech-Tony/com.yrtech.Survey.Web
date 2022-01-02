d7c5362cc9adfce8402214a1adb06af6543e6e2e
huoht <huohaitao1@126.com>
2022-01-02 11:55:48 +0800

    update

---
+++ com.yrtech.SurveyWeb/Controllers/WechatOAuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    public class WechatOAuthController : Controller
+    {
+        //
+        // GET: /WechatOAuth/
+        public async Task<ActionResult> Index()
+        {
+            WechatUtils wechatUtil = new WechatUtils();
+            ReturnJson json = await wechatUtil.GetRedirectUrl();
+            return Redirect(json.Res.Data.redirectURL);
+        }
+
+        public ActionResult AutoLogin(string code)
+        {
+            return View();
+        }
+
+       
+    }
+}
