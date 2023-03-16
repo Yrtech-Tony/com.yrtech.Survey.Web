a26ba9234c9be05cd86567c10c0c2093e4059007
Tony <moujunsheng15@163.com>
2023-03-16 23:17:10 +0800

    修改了有效期

--- com.yrtech.SurveyWeb/Scripts/oss-upload-direct/upload.js
+++ com.yrtech.SurveyWeb/Scripts/oss-upload-direct/upload.js
@@ -32,7 +32,7 @@ function OSSClient(options) {
 var osshost;
 function init_uploader(options) {
     var policyText = {
-        "expiration": "2023-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
+        "expiration": "2028-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
         "conditions": [
         ["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
         ]
