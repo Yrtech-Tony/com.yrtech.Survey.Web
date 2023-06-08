4516030af8fff35af4f2d75d7d6738f9f252620e
huoht <huohaitao1@126.com>
2023-06-08 22:35:48 +0800

    feat： 运营中心

---
+++ com.yrtech.SurveyWeb/Controllers/OperateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.yrtech.SurveyWeb.Controllers
+{
+    public class OperateController : Controller
+    {
+        //
+        // GET: /Operate/
+        public ActionResult RecheckStatusIndex()
+        {
+            return View();
+        }
+
+        public ActionResult ScoreOperateIndex()
+        {
+            return View();
+        }
+	}
+}
