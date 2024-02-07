fac702f185d82e9b7c0ecad08b858be819132c4f
huoht <huohaitao1@126.com>
2024-02-07 21:57:31 +0800

    feat: 1

--- com.yrtech.SurveyWeb/Scripts/api/common.js
+++ com.yrtech.SurveyWeb/Scripts/api/common.js
@@ -281,22 +281,6 @@ function bindLabel(labelType) {
         data.forEach(function (label) {
             $("#LabelId").append($("<option>").val(label.LabelId).text(label.LabelName));
         })
-    })
-    $.ajaxSettings.async = true;
-}
-//  绑定标签-通用 多个
-function bindLabelMultiple(labelType) {
-    $.ajaxSettings.async = false;
-    $.commonGet("Master/GetLabel", {
-        brandId: $("#brand-sel").val(),
-        labelId: '',
-        labelType: labelType,
-        useChk: true
-    }, function (data) {
-        $("#LabelId").append($("<option>").val('').text('请选择'));
-        data.forEach(function (label) {
-            $("#LabelId").append($("<option>").val(label.LabelId).text(label.LabelName));
-        })
         $("#LabelId").addClass("selectpicker").prop("title", "").prop("multiple", true).data("live-search", true).selectpicker("refresh");
     })
     $.ajaxSettings.async = true;
